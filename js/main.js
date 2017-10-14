51520815cf713ceceba2e0d8f38ad13ad6577062
pbhattacharyya <31600118+pbhattacharyya@users.noreply.github.com>
2017-10-14 13:25:56 -0400

    small tweak

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-
+function CouchPillow(shape, quantity, color, custom) {
+    this.name = "Couch Pillow"; 
+    this.shape = shape;
+    this.quantity = quantity;
+    this.unitPrice = 29;
+    this.totalPrice = 29 * quantity;
+    this.custom = custom;
+    this.image = "img/couch-pillow-background.jpg";
+  }
 
 
 $(document).ready(function() { 
     console.log("JS");
-
 
 /* When couch pillow box on the product page is clicked */
     $("#couch-pillow-box").click(function() {
@@ -12,5 +19,7 @@ $(document).ready(function() {
         $("#couch-pillow-box .top-panel").css("width", "670px"); /* to enlarge the top panel*/
         $("#couch-pillow-box .backgroundimage").css("max-width", "").css("height", "auto").css("float", "left").css("padding", "10px").css("padding-righ", "30px").css("display", "inline-block").css("margin", "auto").css("max-height", "255px"); /* to resize the image*/
         $("#couch-pillow-box .text-box").css("visibility", "visible"); /* to make the input forms visible*/
+
+        $("#couch-pillow-box").click(function()
     });
 });
