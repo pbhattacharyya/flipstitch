ba6994e48ab5da479267b6d06d3975c14c1780cf
pbhattacharyya <31600118+pbhattacharyya@users.noreply.github.com>
2017-10-14 13:25:40 -0400

    couch pillow button expand

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+$(document).ready(function() { 
+    console.log("JS");
+
+
+/* When couch pillow box on the product page is clicked */
+    $("#couch-pillow-box").click(function() {
+        console.log("couch pillow expand"); /*test*/
+        $("#couch-pillow-box").css("width", "670px").css("cursor", "default"); /* to enlarge the box and change the cursor*/
+        $("#couch-pillow-box .top-panel").css("width", "670px"); /* to enlarge the top panel*/
+        $("#couch-pillow-box .backgroundimage").css("max-width", "").css("height", "auto").css("float", "left").css("padding", "10px").css("padding-righ", "30px").css("display", "inline-block").css("margin", "auto").css("max-height", "255px"); /* to resize the image*/
+        $("#couch-pillow-box .text-box").css("visibility", "visible"); /* to make the input forms visible*/
+    });
+});
